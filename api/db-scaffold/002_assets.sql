c8e4bf1e5a4ab2584a50b6a443329f1704be2b84
Yosi <yosiasz@gmail.com>
2022-07-21 23:33:43 -0700

    chore(api) scaffold api

---
+++ api/db-scaffold/002_assets.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `myfi`.`assets` (
+  `idassets` INT NOT NULL,
+  `name` VARCHAR(50) NULL,
+  PRIMARY KEY (`idassets`));
---
+++ api/db-scaffold/010_banks.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `banks`
+(
+  `id`            INT(11) NOT NULL auto_increment ,
+  `name`          VARCHAR(255) NOT NULL ,
+  `created_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `updated_at`    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`id`),
+  UNIQUE `idx_name_unique` (`name`(255))
+)
+engine = innodb charset=utf8mb4 COLLATE utf8mb4_general_ci;
