e2b2965991b42dfd854278a7cc29b257369747e2
Yosi <yosiasz@gmail.com>
2022-07-26 09:44:03 -0700

    Clean up work

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom'
-ReactDOM.render(
-    /,div>
-    <Navbar />
-    
-    </div,
-    document.getElementById("root")
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
