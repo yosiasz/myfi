52dd1e01ba77cf7189c5f849502c2cf8d6c6a54b
ዮስያስ <yosiasz@gmail.com>
2023-07-05 09:02:09 -0700

    feat(css) Implement css, some clean ups

--- app/src/components/ListBanks.tsx
+++ app/src/components/ListBanks.tsx
@@ -1,39 +1,32 @@
 import { useEffect, useState } from "react";
-import BankService from "../services/banksService";
+import BankService from "../services/bankService";
+import IBankData from "../services/models/Bank";
 
 const ListBanks = () => {
-  const [banks, setBanks] = useState([]);
-
-  /*   
-    useEffect(() => {
-      const fetchBanks = async () => {
-        const banks = await BankService.getBanks();
-        setBanks(banks);
-      };
-    }, []); 
-  */
+  const [banks, setBanks] = useState<IBankData[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     BankService.getBanks().then((data) => setBanks(data));
   }, []);
 
-  const fetchBanks = () => {
-    fetch("http://localhost:5000/banks")
-      .then((res) => res.json())
-      .then((data) => {
-        setBanks(data.results);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {banks.map((bank) => (
-          <li key={bank.id} className="list-group-item">
+        {banks.map((bank, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={bank.id}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {bank.name}
           </li>
         ))}
